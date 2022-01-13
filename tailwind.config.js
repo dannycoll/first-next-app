5e979ebb8514298d06882efde81d315000ed9ad3
Daniel Collins <dptc98@gmail.com>
2022-01-13 20:22:57 +0000

    Styling tips

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    purge: [
+      // Use *.tsx if using TypeScript
+      './pages/**/*.jsx',
+      './components/**/*.jsx',
+      './pages/**/*.tsx',
+      './components/**/*.tsx',
+      './pages/**/*.js',
+      './components/**/*.js',
+      './pages/**/*.ts',
+      './components/**/*.ts'
+    ]
+    // ...
+  }
